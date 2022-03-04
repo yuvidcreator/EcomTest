e118f018d6989f997e0cfd31c7e9faca8949ebcb
unknown <yuvrajl.103@gmail.com>
2022-03-05 04:43:22 +0530

    commit git

--- coreEcom/settings.py
+++ coreEcom/settings.py
@@ -156,12 +156,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'staticfiles'), ]
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'assets'), ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
