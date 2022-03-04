194b1fe2cac8f166693b326e4e7ad3241d9a4656
unknown <yuvrajl.103@gmail.com>
2022-03-05 04:52:18 +0530

    commit git

--- coreEcom/urls.py
+++ coreEcom/urls.py
@@ -25,14 +25,12 @@ from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('ecom.urls')),
-    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    path('', include('ecom.urls')), 
 ]
 
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
